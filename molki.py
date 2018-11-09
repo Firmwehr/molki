2517ba7fd148a0e3be61f35862ed9ef5a6f8e4da
Andreas Fried <andi@0xaf.de>
2018-11-09 23:17:10 +0100

    Fix test case (32 -> 64 bit).

--- molki.py
+++ molki.py
@@ -645,12 +645,15 @@ if True:
     .function minijava_main 0 1
     call __stdlib_malloc [ $40 ] -> %@0
 
-    movq $42, 20(%@0)
-    call __stdlib_println [ 20(%@0) ]
+    movq $42, 16(%@0)
+    call __stdlib_println [ 16(%@0) ]
 
     movq $24, %@1
     movq $23, (%@0,%@1)
     call __stdlib_println [ (%@0, %@1) ]
+
+    addq [ 16(%@0) | (%@0, %@1) ] -> %@2
+    call __stdlib_println [ %@2 ]
 
     movq $0, %@r0
     """))
